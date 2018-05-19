b1cc94f9b13433171c952a8b745948669dec320a
starrysky51 <38041226+starrysky51@users.noreply.github.com>
2018-05-19 16:24:55 +0900

    数当てゲーム

--- taikutu.py
+++ taikutu.py
@@ -1,10 +1,20 @@
-def spam(divide_by):
-    try:
-        return 42 / divide_by
-    except ZeroDivisionError:
-        print('エラー：不正な引数です。')
+import random
+secret_number = random.randint(1, 20)
+print('1から20までの数をあててください')
 
-print(spam(2))
-print(spam(12))
-print(spam(0))
-print(spam(1))
+#6回引く
+for guesses_taken in range(1, 7):
+    print('数を入力してください。')
+    guess = int(input())
+
+    if guess < secret_number:
+        print('小さいです')
+    elif guess > secret_number:
+        print('大きいです')
+    else:
+        break
+
+if guess == secret_number:
+    print('当たり！' + str(guesses_taken) + '回で当たりました！')
+else:
+    print('残念。正解は' + str(secret_number) + 'でした')
