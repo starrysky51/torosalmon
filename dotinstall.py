21fc9bb6e6895775e3ab510379ea0ce56705a416
starrysky51 <gz.51.pt@gmail.com>
2018-09-21 20:57:56 +0900

    文字列に値を埋め込む

--- dotinstall.py
+++ dotinstall.py
@@ -1,19 +1,6 @@
-x = 10
-print(x / 3) # 3.33...
-print(x // 3) # 3
-print(x % 3) # 1
-print(x ** 2) # 100
+name = "taguchi"
+score = 52.8
 
-y = 4
-# y = y + 12
-y += 12
-print(y) # 16
+#print("name: %-10s, score: %10.2f" % (name, score))
 
-# and or not
-print(True and False) # False
-print(True or False) # True
-print(not True) # False
-
-# + *
-print("hello" + "world")
-print("hello" *3)
+print("name: {0:>10s}, score: {1:10.2f}".format(name,score))
