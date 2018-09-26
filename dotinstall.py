758298e3b72cd833c0963d8d9b212e566a0df584
starrysky51 <gz.51.pt@gmail.com>
2018-09-26 22:15:18 +0900

    てすと

--- dotinstall.py
+++ dotinstall.py
@@ -1,7 +1,5 @@
-def say_hi(name, age = 20):
-    print("hi {0}, ({1})".format(name, age))
+def func(a, b):
+    return c
 
-say_hi("tom", 23)
-say_hi("bob", 21)
-say_hi("steave")
-say_hi(age = 18, name = "penpen")
+x = func(2, 4)
+print(x) #16
