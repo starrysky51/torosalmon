0ca17ab4ad413bbcab22781280cb5a7ce9e0c629
starrysky51 <gz.51.pt@gmail.com>
2018-11-06 01:14:52 +0900

    for文　ループ処理

---
+++ dotinstall.py/dot9.py
@@ -0,0 +1,13 @@
+# for  in variable
+
+# for 変数　in データの集合：
+#  処理
+
+#for i in range(0, 10):
+for i in range(10):
+    if i == 5:
+        #break
+        continue
+    print(i)
+else:
+    print("end")
