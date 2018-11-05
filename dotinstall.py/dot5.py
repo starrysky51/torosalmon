36dc3f0a8a4346a4ee262428ed4814c96ecca568
starrysky51 <gz.51.pt@gmail.com>
2018-11-06 00:56:35 +0900

    データの演算

---
+++ dotinstall.py/dot5.py
@@ -0,0 +1,21 @@
+# + = * / // % **
+
+x = 10
+print(x /3) #3.333....
+print(x // 3) # 3
+print(x % 3) # 1
+print(x ** 2) # 100
+
+y = 4
+#y = y + 4
+y += 4
+print(y) # 16
+
+# and or # # NOTE:
+print(True and False) #False
+print(True and False) #True
+print(not True) # False
+
+# + *
+print("Hello" + "world")
+print("hello" * 3)
