3b79fe90b0dab6cdd414c511dde3fa38f9a105d8
starrysky51 <gz.51.pt@gmail.com>
2018-11-07 00:53:00 +0900

    クラスの継承

---
+++ dotinstall.py/dot18.py
@@ -0,0 +1,23 @@
+#　クラスの継承
+# User -> AfminUser
+
+class User:
+    def __init__(self, name):
+        self.name = name
+    def say_hi(self):
+        print("hi {0}".format(self.name))
+
+class AdminUser(User):
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+    def say_hello(self):
+        print("hello {0}".format(self.name, self.age))
+    # override
+    def say_hi(self):
+        print("[admin] hi {0}".format(self.name))
+
+bob = AdminUser("bob", 23)
+print(bob.name)
+bob.say_hi()
+bob.say_hello()
