b7c7fca0827ad7b85651467b12997895ca2413c4
starrysky51 <gz.51.pt@gmail.com>
2018-11-07 23:56:39 +0900

    辞書型

---
+++ dotinstall.py/dot27.py
@@ -0,0 +1,13 @@
+# リスト型とタプル
+# 集合型
+# 辞書型
+
+sales = {"taguchi": 200, "fkoji": 400}
+#print(sales["taguchi"])
+#sales["taguchi"] = 300
+#sales["dotinstall"] = 500
+#del(sales["fkoji"])
+#print(sales)
+
+for key, value in sales.items():
+    print("{0}: {1}".format(key, value))
