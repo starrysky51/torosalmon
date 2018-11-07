32d9d5baf330bf7b4efc6a6b4b3d840a5c1b4a9d
starrysky51 <gz.51.pt@gmail.com>
2018-11-07 23:49:59 +0900

    集合型

---
+++ dotinstall.py/dot26.py
@@ -0,0 +1,20 @@
+# リスト型とタプル
+#  集合型
+# 辞書型
+
+# セット
+
+#a = set([5, 4, 8, 5])
+#a = {5, 3, 8, 5}
+#print(a)
+#print(5 in a) # True
+#a.add(2)
+#a.remove(3)
+#print(a)
+#print(len(a))
+
+a = {1, 3, 5, 8}
+b = {3, 5, 8, 9}
+print(a | b)
+print(a & b)
+print(a - b)
