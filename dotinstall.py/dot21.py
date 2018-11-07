57f895d00fc21694154a0e475450b85c80b47ea5
starrysky51 <gz.51.pt@gmail.com>
2018-11-07 15:51:28 +0900

    パッケージでモジュール管理

---
+++ dotinstall.py/dot21.py
@@ -0,0 +1,13 @@
+# モジュール
+
+
+#import mypackage.dot20User
+import mypackage.dot20User import AdminUser
+
+bob = mypackage.dot20User.AdminUser("bob", 23)
+
+tom = User("tom")
+
+print(bob.name)
+bob.say_hi()
+bob.say_hello()
