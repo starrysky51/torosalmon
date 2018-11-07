b265ed3eb1606aefb845647c8e573ee188eeb7ab
starrysky51 <gz.51.pt@gmail.com>
2018-11-07 23:44:29 +0900

    スライス

---
+++ dotinstall.py/dot25.py
@@ -0,0 +1,13 @@
+# リスト型とタプル
+# -スライス
+# 集合型
+# 辞書型
+
+#scores = [40, 50, 70, 90, 60]
+#print(scores[1:4]) # 50, 70, 90
+#print(scores[:2]) # 40, 50
+#print(scores[3:]) #90, 60
+#print(scores[-3:]) # 70, 90, 60
+
+s = "hello"
+print(s[1:4])
